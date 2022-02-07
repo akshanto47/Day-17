9fd77882d719772a3d784bcfacee26189a23599e
Abdul Khalek <63781173+akshanto47@users.noreply.github.com>
2022-02-07 17:33:40 +0600

    Add files via upload
    Day-17

---
+++ Day-17/app/classes/Example.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Example
+{
+
+
+    public $i;
+
+    public function index()
+    {
+        /*
+            * Single
+            * Condition
+                   *if
+                   * if else
+                   * if else if
+                   *
+            * Repeated
+                   * for
+                   * while
+                   * do while
+                   * foreach
+        */
+
+//
+//        for ($this->i = 45; $this->i > 22; $this->i--)
+//        {
+//            echo 'This number is = '.$this->i. " <br/>";
+//        }
+
+//        $this->i = 10;
+//        while ($this->i < 20)
+//        {
+//            echo 'This number is = '. $this->i.'<br/>';
+//            $this->i++;
+//        }
+//
+
+//
+
+        
+    }
+}
